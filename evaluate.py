1d2638e7ef582cdd0074973c77476e3479795a75
AxelEriksson <axeer739@student.liu.se>
2024-12-27 14:56:25 +0100

    Update evaluate.py
    trying to visualize result of different models using tensorboard

--- evaluate.py
+++ evaluate.py
@@ -2,11 +2,12 @@ import argparse
 from env import droneEnv
 import logging
 from stable_baselines3 import A2C
-from stable_baselines3 import DQN
+#from stable_baselines3 import DQN
 
 #Initialize the argument parser
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=str, default='models/125k.zip', help='Path to the model zip file')
+parser.add_argument('--log_dir', type=str, default='evaluation_logs', help='Directory to save evaluation logs')
 args = parser.parse_args()
 
 try:
@@ -20,8 +21,20 @@ except Exception as e:
     logger.error(e)
     quit()
 
-import os
-from stable_baselines3.common.evaluation import evaluate_policy
+# Initialize TensorBoard writer
+writer = SummaryWriter(log_dir=args.log_dir)
 
-evaluate_policy(model, env, n_eval_episodes=10)
-quit()
+# Evaluate the model
+mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=10, return_episode_rewards=True)
+
+# Log evaluation results to TensorBoard
+for i, reward in enumerate(mean_reward):
+    writer.add_scalar('Evaluation/Reward', reward, i)
+
+writer.close()
+
+#import os
+#from stable_baselines3.common.evaluation import evaluate_policy
+
+#evaluate_policy(model, env, n_eval_episodes=10)
+#quit()
